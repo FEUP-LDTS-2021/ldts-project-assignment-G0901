61eaa97dcf2071cdcff983b84279351a988e42d2
andrefcbarbosa <72624261+andrefcbarbosa@users.noreply.github.com>
2022-01-12 18:06:14 +0000

    Created game model

---
+++ src/main/java/model/GameModel.java
@@ -0,0 +1,30 @@
+package model;
+
+public class GameModel implements Model{
+    CarModel carModel;
+    TrackModel trackModel;
+    public GameModel(CarModel car, TrackModel track){
+        carModel = car;
+        trackModel = track;
+    }
+
+    public GameModel() {
+
+    }
+
+    public CarModel getCarModel() {
+        return carModel;
+    }
+
+    public TrackModel getTrackModel() {
+        return trackModel;
+    }
+
+    public void setCarModel(CarModel carModel) {
+        this.carModel = carModel;
+    }
+
+    public void setTrackModel(TrackModel trackModel) {
+        this.trackModel = trackModel;
+    }
+}
