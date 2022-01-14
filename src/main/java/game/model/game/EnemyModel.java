25e531d56834929dd0a5ba63c7ce587dfd4ed665
andrefcbarbosa <72624261+andrefcbarbosa@users.noreply.github.com>
2022-01-14 10:54:49 +0000

    Created Interface for enemymodel

---
+++ src/main/java/game/model/game/EnemyModel.java
@@ -0,0 +1,29 @@
+package game.model.game;
+
+import game.model.Position;
+
+
+public class EnemyModel {
+    Position position;
+    int lane;
+
+    public EnemyModel()
+    {
+
+    }
+
+    public EnemyModel(Position position)
+    {
+
+    }
+
+    public Position getPosition()
+    {
+        return position;
+    }
+
+    public void getCloser(double speed)
+    {
+
+    }
+}
---
+++ src/test/java/game/model/EnemyModelTest.java
@@ -0,0 +1,9 @@
+package game.model;
+
+import game.model.game.EnemyModel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class EnemyModelTest {
+
+}
