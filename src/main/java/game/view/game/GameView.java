2a233477cf86c7f6fee21a4e8c264f1c23cb431b
andrefcbarbosa <72624261+andrefcbarbosa@users.noreply.github.com>
2022-01-14 19:35:50 +0000

    Edited game view with enemy

--- src/main/java/game/view/game/GameView.java
+++ src/main/java/game/view/game/GameView.java
@@ -5,21 +5,25 @@ import game.model.game.GameModel;
 import game.view.View;
 
 import java.io.IOException;
+import java.util.Vector;
 
 public class GameView extends View<GameModel> {
     public TrackView track_view; //TODO Change views into subclass
     public CarView car_view;
+    public EnemyView enemy_view;
 
     public GameView(GameModel model) {
         super(model);
         this.track_view = new TrackView();
         this.car_view = new CarView();
+        this.enemy_view = new EnemyView();
     }
 
     @Override
     public void drawElements(GUI gui) throws IOException {
         track_view.draw(gui.getGraphics(), model.getTrackModel());
         car_view.draw(gui.getGraphics(), model.getCarModel());
+        enemy_view.draw(gui.getGraphics(), model.getEnemyModel());
         gui.refresh();
 
     }
