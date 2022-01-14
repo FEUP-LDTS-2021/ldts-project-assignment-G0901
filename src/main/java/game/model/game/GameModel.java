1174eb01e0e0bead10c7dcacd21553e17cd28e22
andrefcbarbosa <72624261+andrefcbarbosa@users.noreply.github.com>
2022-01-14 12:10:34 +0000

    Updated game model

--- src/main/java/game/model/game/GameModel.java
+++ src/main/java/game/model/game/GameModel.java
@@ -5,15 +5,18 @@ import game.model.Model;
 public class GameModel implements Model {
     CarModel carModel;
     TrackModel trackModel;
+    EnemyModel enemyModel;
 
-    public GameModel(CarModel car, TrackModel track){
+    public GameModel(CarModel car, TrackModel track, EnemyModel enemy){
         carModel = car;
         trackModel = track;
+        enemyModel = enemy;
     }
 
     public GameModel() {
         carModel = new CarModel();
         trackModel = new TrackModel();
+        enemyModel = new EnemyModel();
     }
 
     public CarModel getCarModel() {
@@ -24,11 +27,17 @@ public class GameModel implements Model {
         return trackModel;
     }
 
+    public EnemyModel getEnemyModel() {return enemyModel; }
+
     public void setCarModel(CarModel carModel) {
         this.carModel = carModel;
     }
 
     public void setTrackModel(TrackModel trackModel) {
         this.trackModel = trackModel;
+    }
+
+    public void setEnemyModel(EnemyModel enemyModel) {
+        this.enemyModel = enemyModel;
     }
 }
