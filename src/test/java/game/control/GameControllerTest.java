03baac83ec64ffc648dbf9dd11e756cdc467b046
José Luís Cunha Rodrigues <joseluiscunharodrigues@gmail.com>
2022-01-17 15:19:36 +0000

    Added collision tests

--- src/test/java/game/control/GameControllerTest.java
+++ src/test/java/game/control/GameControllerTest.java
@@ -6,6 +6,7 @@ import game.control.game.EnemyController;
 import game.control.game.GameController;
 import game.control.game.TrackController;
 import game.gui.GUI;
+import game.model.game.CarModel;
 import game.model.game.GameModel;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -14,12 +15,16 @@ import org.mockito.Mockito;
 
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 public class GameControllerTest {
     private GameController controller;
     private TrackController trackController;
     private CarController carController;
     private EnemyController enemyController;
     private GameModel game_model;
+    private CarModel car, enemy;
     private Application app;
     private GUI gui;
 
@@ -47,5 +52,58 @@ public class GameControllerTest {
         Mockito.verify(carController, Mockito.times(1)).step(app, gui.getAction());
         Mockito.verify(enemyController, Mockito.times(1)).step(app,gui.getAction());
 
+    }
+
+    void setUpCollision() {
+        car = Mockito.mock(CarModel.class);
+        enemy = Mockito.mock(CarModel.class);
+
+        when(car.getLane()).thenReturn(0);
+        when(enemy.getLane()).thenReturn(0);
+
+        when(car.getHeight()).thenReturn(5);
+        when(enemy.getHeight()).thenReturn(5);
+
+        when(car.getY()).thenReturn(10);
+    }
+
+    @Test
+    void testCollision1() {
+        setUpCollision();
+        when(enemy.getY()).thenReturn(0);
+
+        Boolean collision = controller.checkCollision(car, enemy);
+
+        assertFalse(collision);
+    }
+
+    @Test
+    void testCollision2() {
+        setUpCollision();
+        when(enemy.getY()).thenReturn(9);
+
+        Boolean collision = controller.checkCollision(car, enemy);
+
+        assertTrue(collision);
+    }
+
+    @Test
+    void testCollision3() {
+        setUpCollision();
+        when(enemy.getY()).thenReturn(14);
+
+        Boolean collision = controller.checkCollision(car, enemy);
+
+        assertTrue(collision);
+    }
+
+    @Test
+    void testCollision4() {
+        setUpCollision();
+        when(enemy.getY()).thenReturn(20);
+
+        Boolean collision = controller.checkCollision(car, enemy);
+
+        assertFalse(collision);
     }
 }
