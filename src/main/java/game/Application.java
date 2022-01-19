def470d27442e83bdd8a124b1d35881e42779e83
Guilherme Almeida <72418555+theguilhermealmeida@users.noreply.github.com>
2022-01-19 11:42:11 +0000

    Added Observers list and addObserver Method

--- src/main/java/game/Application.java
+++ src/main/java/game/Application.java
@@ -3,28 +3,38 @@ package game;
 import com.googlecode.lanterna.TerminalSize;
 import game.gui.GUI;
 import game.model.menu.MenuModel;
+import game.observer.Music;
+import game.observer.Observer;
 import game.states.menu.MenuState;
 import game.states.State;
 
+import javax.sound.sampled.LineUnavailableException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     private TerminalSize size = new TerminalSize(180,80);
     private State state;
     private GUI gui;
+    Music music;
+    List<Observer> observers;
 
-    public Application() {
+    public Application() throws LineUnavailableException {
+        observers = new ArrayList<>();
         this.state = new MenuState(new MenuModel(size));
-        //this.state = new SelectTrackState(new SelectTrackModel(size));
-        //his.state = new GameState(new GameModel(size));
         this.gui = new GUI(size);
+        music = new Music(this);
+
+
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, LineUnavailableException {
         new Application().run();
     }
 
     private void run() throws IOException {
+        setState(state);
         int FPS = 1;
         int frameTime = 60 / FPS;
 
@@ -43,12 +53,23 @@ public class Application {
 
         gui.getScreen().stopScreen();
         System.exit(0);
-
     }
 
     public void setState(State state) {
         this.state = state;
+
     }
 
+    public State getState() {
+        return state;
+    }
+
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public List<Observer> getObservers() {
+        return observers;
+    }
 
 }
